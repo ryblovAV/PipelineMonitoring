8d620e242c13046c5d0a41ecb44c97546aea7757
Ryblov <ryblov.a.v@gmail.com>
2021-12-17 09:41:56 +0300

    added frontend

--- backend/src/main/scala/monitoring/routes/PipelineRoutes.scala
+++ backend/src/main/scala/monitoring/routes/PipelineRoutes.scala
@@ -4,10 +4,10 @@ import cats.effect.Sync
 import cats.syntax.all._
 import monitoring.domain.Codecs._
 import monitoring.services.Pipelines
-import org.http4s.HttpRoutes
 import org.http4s.circe.CirceEntityEncoder._
 import org.http4s.dsl.Http4sDsl
 import org.http4s.server.Router
+import org.http4s.{Header, HttpRoutes}
 
 
 final case class PipelineRoutes[F[_]: Sync](pipelines: Pipelines[F]
@@ -16,7 +16,11 @@ final case class PipelineRoutes[F[_]: Sync](pipelines: Pipelines[F]
   private val prefixPath = "/pipelines"
 
   private val httpRoutes = HttpRoutes.of[F] {
-    case GET -> Root => Ok(pipelines.getAllJobs)
+    case GET -> Root =>
+      Ok(pipelines.getAllJobs)
+        .map(response =>
+          response.copy(headers = response.headers.put(Header("Access-Control-Allow-Origin", "*")))
+        )
     case req @ POST -> Root / "add"  =>
       req.as[String].flatMap(jobName => Ok(pipelines.addJob(jobName)))
   }
